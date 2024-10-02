9dccc487cf7205f9c9e1e229b9fa2d3ed9cc8c52
Diego Arbeláez <diegoandresarbelaez@gmail.com>
2024-10-02 06:24:00 -0500

    Feature: Project Setup (#1)
    * Add internet permission
    
    * Setup routes and theme project
    
    * Move constants directory
    
    * Add responsive widget
    
    * Add error handler

---
+++ lib/config/navigation/go_router.dart
@@ -0,0 +1,13 @@
+import 'package:go_router/go_router.dart';
+import 'package:three_planets/src/presentation/pages/home_screen.dart';
+
+/// Get the routes used by navigate in the app
+var appRoutes = GoRouter(
+  initialLocation: HomeScreen.path,
+  routes: [
+    GoRoute(
+      path: HomeScreen.path,
+      pageBuilder: (_, __) => const NoTransitionPage(child: HomeScreen()),
+    ),
+  ],
+);
---
+++ lib/utils/utils.dart
@@ -0,0 +1,6 @@
+
+import 'package:flutter/material.dart';
+
+double widthScreen(BuildContext context) => MediaQuery.sizeOf(context).width;
+
+double heightScreen(BuildContext context) => MediaQuery.sizeOf(context).height;
