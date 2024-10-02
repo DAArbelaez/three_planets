cf400f42d5ad7229fb879707b52cf58b837ba282
DAArbelaez <diegoandresarbelaez@gmail.com>
2024-10-02 06:04:50 -0500

    Setup routes and theme project

---
+++ lib/config/theme/app_theme.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:three_planets/utils/constants/app_color_constants.dart';
+
+final lightTheme = ThemeData(
+  fontFamily: 'Poppins',
+  colorScheme: const ColorScheme.light(
+    primary: kAmaranthPrimary,
+    secondary: kMyrtleGreen,
+    onPrimary: kWhite,
+    onSecondary: kWhite,
+    onTertiary: kWhite,
+    surface: kWhite,
+    onSurface: kMyrtleGreen,
+    error: kAmaranthPrimary,
+    surfaceTint: Colors.transparent,
+  ),
+);
