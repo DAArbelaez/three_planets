ef7f71cba08976adfa5dd0f226bd9b70710da9bb
DAArbelaez <diegoandresarbelaez@gmail.com>
2024-10-02 12:04:22 -0500

    Create planet list

--- lib/constants/tp_constants.dart
+++ lib/constants/tp_constants.dart
@@ -1,3 +1,6 @@
 import 'package:flutter/material.dart' show EdgeInsets;
 
 const kPagePadding = EdgeInsets.symmetric(horizontal: 20.0);
+
+// -- Host
+String kUrlBase = 'https://us-central1-a-academia-espacial.cloudfunctions.net';
